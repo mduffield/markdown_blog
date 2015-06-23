36354f8c7429931e1d53c769726b8f350b0df3b2
sig2noise <sig2noise@merlin.sig2noise.net>
2015-06-23 10:19:02 -0600

    Adding readme, linking posts, and adding htaccess sample

---
+++ README.md
@@ -0,0 +1,7 @@
+#Markdown Blog
+
+1.  cp htaccess.sample .htaccess
+2.  modify .htaccess to have proper path for rewrite base param
+3.  modify index.php with correct values for site_name, company, etc
+
+
--- classes/post.php
+++ classes/post.php
@@ -5,7 +5,7 @@ require_once("vendor/php-markdown-lib/Michelf/Markdown.inc.php");
 class Post{
   function __construct($filename){
     $file = file("posts/" . $filename);
-    //Get metadata
+    //Get/set metadata
     $this->title = str_replace("\n", "", $file[0]);
     $this->date = str_replace("\n", "", substr($file[1], 2));
     $this->description = str_replace("\n", "", $file[2]);
@@ -16,7 +16,7 @@ class Post{
     $components_of_date[4] = substr($components_of_date[2], 6,7); //Get minutes
     $components_of_date[2] = substr($components_of_date[2], 0,2); //Get day
     $this->timestamp = mktime($components_of_date[3], $components_of_date[4], 0, $components_of_date[1], $components_of_date[2], $components_of_date[0]);
-    $this->url = BASE_URL.rawurlencode(substr($filename, 0, -3));
+    $this->url = rawurlencode(substr($filename, 0, -3));
     $this->filename = $filename;
     //Empty it!
     $file[0] = "";
